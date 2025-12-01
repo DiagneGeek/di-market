ff74f0700b7a1052fb47c16a22c3da9209e0c5d8
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-01 21:09:14 +0000

    Modify product page styles and layout
    Updated image class and adjusted description padding.

--- src/routes/products/[slug]/+page.svelte
+++ src/routes/products/[slug]/+page.svelte
@@ -18,14 +18,14 @@
   <img 
      src={product.image}  
      alt={product.title}
-     class="rounded-2xl border-2 border-card my-4 w-full"
+     class="rounded-2xl border-2 border-card my-4 w-full min-h-[150px]"
   />
   <h1>{product.title}</h1>
   <div class="px-2 py-4 flex justify-between rounded-2xl border-1 border-card">
     <p class="font-bold italic">{product.price} fcfa</p>
     <p>Categorie: {product.category}</p>
   </div>
-  <p class="my-2 bg-card rounded-2xl text-right p-2">{product.description}</p>
+  <p class="my-2 bg-card rounded-2xl text-right px-2 py-4">{product.description}</p>
   <a href="https://wa.me/{product.Sellers?.phone}?text=Salut {product.Sellers?.name}, je suis interessé par votre produit sur DiMarket '{product.title}'. On peut en discuter ?" target="_blank" rel="noopener norefferer">
   <Button class="my-4">Contacter le vendeur</Button>
 </a>
