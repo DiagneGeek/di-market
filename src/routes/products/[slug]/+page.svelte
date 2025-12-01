5bbdacafdfc31cceef2a9ae934584a1453c1ca66
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-01 21:11:03 +0000

    Update +page.svelte

--- src/routes/products/[slug]/+page.svelte
+++ src/routes/products/[slug]/+page.svelte
@@ -25,7 +25,7 @@
     <p class="font-bold italic">{product.price} fcfa</p>
     <p>Categorie: {product.category}</p>
   </div>
-  <p class="my-2 bg-card rounded-2xl text-right px-2 py-4">{product.description}</p>
+  <p class="my-2 mt-4 bg-card rounded-2xl text-right px-2 py-4">{product.description}</p>
   <a href="https://wa.me/{product.Sellers?.phone}?text=Salut {product.Sellers?.name}, je suis interessé par votre produit sur DiMarket '{product.title}'. On peut en discuter ?" target="_blank" rel="noopener norefferer">
   <Button class="my-4">Contacter le vendeur</Button>
 </a>
