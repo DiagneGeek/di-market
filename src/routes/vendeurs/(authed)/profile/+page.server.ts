76202f1b1b43629c8b84e8f768d58fbce7488778
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-02 20:14:10 +0000

    Fix cookie removal for session ID

--- src/routes/vendeurs/(authed)/profile/+page.server.ts
+++ src/routes/vendeurs/(authed)/profile/+page.server.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const actions = {
   disconnect: async ({cookies}) => {
-    cookies.remove("sessionid")
+    cookies.remove("session_id")
     redirect(303, "/vendeurs")
   }
 }
