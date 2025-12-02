8df6f5fab1ed204c161ab28d11473202ca0a111c
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-02 19:41:43 +0000

    Add Hero component and seller greeting to page

--- src/routes/boutiques/[seller_id]/+page.svelte
+++ src/routes/boutiques/[seller_id]/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Vendeurs</h1>
+<script lang="ts">
+  import Hero from "$lib/components/Hero.svelte"
+  import ArticleCard from "$lib/components/ArticleCard.svelte
+
+  const {data} = $props()
+</script>
+
+<Hero>
+<h1>
+  Bienvenue dans la boutique de 
+  <span class="highlight">
+   {data?.seller?.name}
+  </span>
+</h1>
+</Hero>
+
+<h2 class="mb-4">Mes produits</h2>
+
+
