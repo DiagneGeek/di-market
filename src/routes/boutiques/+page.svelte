9a4696db8a2aac25598f4b994f2017dae812b41e
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-02 19:26:09 +0000

    Add boutique page with seller listings

---
+++ src/routes/boutiques/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Hero from "$lib/components/Hero.svelte";
+  const sellers = []
+</script>
+
+<Hero>
+<h1>Decouvrez Nos Illustres Vendeurs</h1>
+</Hero>
+
+<section class="flex justify-center gap-4 flex-wrap">
+  {#each sellers as seller}
+   <div class="bg-card p-4 rounded-2xl shadow w-[300px] flex flex-col gap-6">
+     <h2 class="text-secondary">{seller.name}</h2>
+     <p class="flex p-2 rounded-xl bg-bg-2 justify-between">
+       <span>Telephone:</span>
+       <span>{seller.phone}</span>
+     </p>
+
+     <a 
+       class="highlight"
+         href="/boutiques/{seller.name}?id={seller.id}"
+    >
+       Decouvrir sa boutique
+      </a>
+   </div>
+  {/each}
+</section>
