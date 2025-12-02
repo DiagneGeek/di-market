c2d6b5a81429a6072c006527115b648e949b2eed
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-02 19:44:38 +0000

    Correct seller data retrieval and error handling
    Fix seller data assignment and error handling in load function.

--- src/routes/boutiques/+page.server.ts
+++ src/routes/boutiques/+page.server.ts
@@ -2,9 +2,16 @@ import type { PageLoad } from './$types';
 import {selectTable} from "$lib/server/supabase"
 
 export const load: PageLoad = async ({ params }) => {
+	let sellers = null
+	let error
+	try {
 	const {data, error} = await selectTable("Sellers", "*")
-  return {
-    sellers: data,
+    sellers = data
+	} catch (err) {
+		error = err
+	}
+ return {
+    sellers,
     error
   }
 };
