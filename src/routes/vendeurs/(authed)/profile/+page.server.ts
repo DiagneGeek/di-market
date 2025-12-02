910888036bdeaa0f21c1e181f04cb380eadff85f
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-02 20:22:42 +0000

    Add disconnect action to remove session cookie

--- src/routes/vendeurs/(authed)/profile/+page.server.ts
+++ src/routes/vendeurs/(authed)/profile/+page.server.ts
@@ -1,8 +1,9 @@
 import { redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
 
 export const actions = {
   disconnect: async ({cookies}) => {
     cookies.remove("session_id")
     redirect(303, "/vendeurs")
   }
-}
+} satisfies Actions 
