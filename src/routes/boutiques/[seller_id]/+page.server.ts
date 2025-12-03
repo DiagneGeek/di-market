60d7cd823c7d1acd701f567d8c04898e87391a78
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-03 01:36:50 +0000

    Fix seller loading logic and error handling

--- src/routes/boutiques/[seller_id]/+page.server.ts
+++ src/routes/boutiques/[seller_id]/+page.server.ts
@@ -3,9 +3,14 @@ import {selectTable} from "$lib/server/supabase"
 
 export const load: PageLoad = async ({ params }) => {
 	const {seller_id} = params
-  const {data, error} = await selectTable("Products")
-  const {data: seller} = await selectTable("Sellers")
+	const {data: seller, error} = await selectTable("Sellers")
          .eq("id", seller_id)
         .single()
+	if (!seller) {
+		throw new Error("Cette boutique n'existe pas !")
+	}
+  const {data, error} = await selectTable("Products")
+	      .eq("seller_id", seller_id)
+  
   return {products: data, seller}
 };
