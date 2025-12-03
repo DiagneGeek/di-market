7d9294e7c570d91b8fde7af9979f89590fde41a7
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-03 01:56:46 +0000

    Update NavBar.svelte

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -6,7 +6,7 @@
  <header class="flex justify-between items-center p-4 border-b border-b-slate-200 backdrop-blur-md fixed top-0 left-0 w-full">
      <a href="/" class="font-bold">
        <img 
-       class="w-32"
+       class="w-24"
        src={logo} alt="DiMarket">
      </a>
 	 <nav class="flex items-center gap-2 justify-end *:text-text">
