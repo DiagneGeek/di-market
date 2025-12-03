510d453e60712648c6499d44afa236f6e59589f8
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-03 01:54:25 +0000

    Update NavBar.svelte

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -3,7 +3,7 @@
     const {children} = $props()
  </script>
  
- <header class="flex justify-between items-center p-4 border-b border-b-slate-200 backdrop-blur-md fixed top-0 w-full">
+ <header class="flex justify-between items-center p-4 border-b border-b-slate-200 backdrop-blur-md fixed top-0 left-0 w-full">
      <a href="/" class="font-bold">
        <img 
        class="w-32"
