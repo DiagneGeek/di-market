49ecf09894c7cfab6e217abd4dafba20b2d065f2
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-05 19:36:09 +0000

    Update +server.ts

--- src/routes/vendeurs/(authed)/dashboard/api/add/+server.ts
+++ src/routes/vendeurs/(authed)/dashboard/api/add/+server.ts
@@ -1,5 +1,6 @@
 
 import {addArticle} from "$lib/server/articles"
+import {uploadImage, getPublicUrl} from "$lib/server/supabase"
 import { json } from "@sveltejs/kit"
 import type { RequestHandler } from './$types'
 
@@ -9,10 +10,18 @@ export const POST: RequestHandler = async ({request}) => {
         const slug = data.title?.toLowerCase()
                 .replace(/\s+/g, "-") + Date.now()
 
+        const imageName = `${data.seller_id}-${slug}-${file.name}`
+
+        const {data, error} = await uploadImage("product-images", imageName, data.image)
+        const {data: {publicUrl}} = getPublicUrl("product-images", imageName)
+
         const newArticle = {
             ...data,
+            image: publicUrl,
             slug
         }
+
+        
 
         const adding = await addArticle(newArticle)
 
