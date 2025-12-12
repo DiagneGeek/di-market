8c0423018dcf732f140ee4d082701075c5d14a22
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-12 18:28:12 +0000

    integrated posthog

---
+++ src/routes/+layout.ts
@@ -0,0 +1,17 @@
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
+export const load = async () => {
+  if (browser) {
+      posthog.init(
+        'phc_9jWIHnBxG3UB30XPaefs17euzBn9bBiXlsu2UWShlSP',
+        {
+            api_host: 'https://us.i.posthog.com',
+            defaults: '2025-11-30',
+            person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+        }
+      )
+   }
+
+    return
+};
