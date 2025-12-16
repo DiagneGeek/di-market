4c1af925414f797481cfa05ad08889485667633f
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2025-12-16 21:12:52 +0000

    fixed share ro a friend button

--- src/routes/vendeurs/(authed)/dashboard/+page.svelte
+++ src/routes/vendeurs/(authed)/dashboard/+page.svelte
@@ -12,7 +12,7 @@
   const products: Article[] | any = data.products
   let hasFile = $state(false)
 
-  const message = "Si tu veux créer t1 boutique en ligne gratuitement sans commision, clique sur le lien: https://dimarket.biz/vendeurs"
+  const message = "Si tu veux créer ta boutique en ligne gratuitement sans commision, clique sur le lien: https://dimarket.biz/vendeurs"
     
   let modalIsOpen = $state(false)
 
@@ -88,7 +88,7 @@
 <a href="/boutiques/{data.user.id}" class="my-4 flex justify-center items-center">
   <Button variant="neutral">Voir Ma Boutique</Button>
 </a>
-<a target="_blank" href="https://wa.me/text={message}">
+<a target="_blank" href="https://wa.me?text={message}">
 <Button variant="secondary">Montrer DiMarket à un ami(e)</Button>
 </a>
 
