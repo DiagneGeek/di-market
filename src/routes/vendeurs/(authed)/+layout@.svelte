7f8df5d96d451ecefaf27897762de9587034485f
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2026-02-12 05:22:54 +0000

    Add PostHog initialization and user identification
    Initialize PostHog for analytics and identify users based on URL parameters.

--- src/routes/vendeurs/(authed)/+layout@.svelte
+++ src/routes/vendeurs/(authed)/+layout@.svelte
@@ -3,6 +3,26 @@
 	import NavBar from '$lib/components/NavBar.svelte';
   import {page} from "$app/stores"
 	import Toast from '$lib/components/Toast.svelte';
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
+  if (browser) {
+      posthog.init(
+        'phc_9jWIHnBxG3UB30XPaefs17euzBn9bBiXlsu2UWShlSP',
+        {
+            api_host: 'https://us.i.posthog.com',
+            defaults: '2025-11-30',
+            person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
+        }
+      )
+     if ($page.url.searchParams.has("identified")) {
+        const search = $page.url.searchParams
+        posthog.identify("seller-"+search.get("phone"), {
+          phone: search.get("phone),
+          collection: search.get("collection")
+		})
+	 }
+   }
 
   
   const {children, data} = $props()
